e3b1baf049db0d23def87d1b3ea731df4f30e431
TaeBbong <mok05289@korea.ac.kr>
2020-04-05 00:20:20 +0900

    Upload lecture note

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app_test/screen/screen_home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Quiz App',
+      home: HomeScreen(),
+    );
+  }
+}
